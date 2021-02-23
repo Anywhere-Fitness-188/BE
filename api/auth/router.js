2583e1b2193fd8c3a5da7aa3af823607d7533f68
Jacob Morris <jacobthomasmorris@gmail.com>
2021-02-23 13:25:11 -0700

    display error

--- api/auth/router.js
+++ api/auth/router.js
@@ -21,7 +21,7 @@ router.post('/register', (req, res) => {
       else{
         const hash = bcrypt.hashSync(req.body.password, 2);
         req.body.password = hash;
-        insert(req.body).then(a => res.send(a)).catch(e => res.status(409).send({message: 'username taken'}));
+        insert(req.body).then(a => res.send(a)).catch(e => res.status(409).send({message: e}));
       }
   }
 });
